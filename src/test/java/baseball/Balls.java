30135bf116c752bf13f64d87bbae4e478daa153c
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-15 17:01:58 +0900

    feat: Integer 타입의 List를 전달 받아서 비교 후 저장하는 기능을 추가

--- src/test/java/baseball/Balls.java
+++ src/test/java/baseball/Balls.java
@@ -5,12 +5,19 @@ import java.util.List;
 
 public class Balls {
     private List<Ball> answerBalls;
+    private ResultBall result;
 
     public Balls(List<Integer> values) {
-        answerBalls = new ArrayList<>();
+        result = new ResultBall();
+        answerBalls = integersToBalls(values);
+    }
+
+    private List<Ball> integersToBalls(List<Integer> values) {
+        List<Ball> balls = new ArrayList<>();
         for (int postion = 0; postion < values.size(); postion++) {
-            answerBalls.add( new Ball( postion + 1, values.get(postion) ));
+            balls.add( new Ball( postion + 1, values.get(postion) ));
         }
+        return balls;
     }
 
     public BallStatus play(Ball userBall) {
@@ -21,4 +28,35 @@ public class Balls {
                     .orElse(BallStatus.NOTTING);
     }
 
+    public void playBall(List<Integer> values) {
+        List<Ball> userBalls = integersToBalls(values);
+        for( Ball userBall : userBalls ) {
+            saveResult(this.play(userBall));
+        }
+    }
+    private void saveResult(BallStatus status) {
+        if( status == BallStatus.STRIKE ) {
+            result.setStrike(result.getStrike() + 1);
+        }
+        
+        if( status == BallStatus.BALL ) {
+            result.setBall(result.getBall() + 1);
+        }
+    }
+    public List<Ball> getAnswerBalls() {
+        return answerBalls;
+    }
+    
+    public void setAnswerBalls(List<Ball> answerBalls) {
+        this.answerBalls = answerBalls;
+    }
+
+    public ResultBall getResultBall() {
+        return result;
+    }
+
+    public void setResultBall(ResultBall resultBall) {
+        this.result = resultBall;
+    }
+    
 }
