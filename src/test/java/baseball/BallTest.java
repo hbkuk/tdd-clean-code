83ef6cff34f36477777d54867bd03ffdda1221ed
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-15 13:31:32 +0900

    test: Balls와 Ball를 비교해서 strike, ball, notting을 반환하는 테스트 구현

--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -3,6 +3,8 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +32,32 @@ class BallTest {
         Ball answerBall = new Ball(1, 3);
         Ball userBall = new Ball(1, 3);
         assertThat(answerBall.play(userBall)).isEqualTo(BallStatus.STRIKE);
+    }
+    
+    @Test
+    @DisplayName("Balls와 Ball을 비교해서 Notting을 반환")
+    void balls_vs_ball_notting() {
+        Balls answerBalls = new Balls(Arrays.asList(1,2,3));
+        Ball userBall = new Ball(1, 4);
+        
+        assertThat(answerBalls.play(userBall)).isEqualTo(BallStatus.NOTTING);
+    }
+    
+    @Test
+    @DisplayName("Balls와 Ball을 비교해서 Ball을 반환")
+    void balls_vs_ball_ball() {
+        Balls answerBalls = new Balls(Arrays.asList(1,2,3));
+        Ball userBall = new Ball(3, 1);
+        
+        assertThat(answerBalls.play(userBall)).isEqualTo(BallStatus.BALL);
+    }
+    
+    @Test
+    @DisplayName("Balls와 Ball을 비교해서 Strike을 반환")
+    void balls_vs_ball_strike() {
+        Balls answerBalls = new Balls(Arrays.asList(1,2,3));
+        Ball userBall = new Ball(3, 3);
+        
+        assertThat(answerBalls.play(userBall)).isEqualTo(BallStatus.STRIKE);
     }
 }
