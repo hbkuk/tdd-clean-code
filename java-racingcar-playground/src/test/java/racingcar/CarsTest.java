01fb3c9bf685a3a3a9f25301baf69ace100a827b
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-21 14:49:09 +0900

    test(Cars): Cars 클래스를 생성할때 사용하는 Util 클래스에 대한 테스트 코드 추가

--- java-racingcar-playground/src/test/java/racingcar/CarsTest.java
+++ java-racingcar-playground/src/test/java/racingcar/CarsTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.util.Arrays;
 
@@ -16,5 +17,22 @@ class CarsTest {
         assertThat( newCars.getCars().get(1).getName() ).isEqualTo("crong");
         assertThat( newCars.getCars().get(2).getName() ).isEqualTo("honi");
     }
-
+    
+    @Test
+    void Cars_생성_문자열_쉼표로_구분해서_전달() {
+        String names = "pobi,crong,honi";
+        Cars newCars = new Cars(CarsGenerateUtils.toList(names));
+        assertThat( newCars.getCars().size() ).isEqualTo(3);
+        assertThat( newCars.getCars().get(0).getName() ).isEqualTo("pobi");
+        assertThat( newCars.getCars().get(1).getName() ).isEqualTo("crong");
+        assertThat( newCars.getCars().get(2).getName() ).isEqualTo("honi");
+    }
+    
+    @Test
+    void Cars_생성_문자열_쉼표로_구분하지_않고_전달() {
+        String names = "pobi:crong:honi";
+        assertThatExceptionOfType(RuntimeException.class)
+        .isThrownBy( () -> {new Cars(CarsGenerateUtils.toList(names));})
+        .withMessageMatching("구분자로 이름을 나누어서 입력해야 합니다.");
+    }
 }
