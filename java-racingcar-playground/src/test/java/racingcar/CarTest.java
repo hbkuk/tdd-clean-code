e543b03009927831e3251273e08f1442309a048c
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-21 15:05:53 +0900

    test(Car): move() 메서드 호출 시 인자로 전달하는 정수 값에 관련한 테스트 코드 추가

--- java-racingcar-playground/src/test/java/racingcar/CarTest.java
+++ java-racingcar-playground/src/test/java/racingcar/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,20 @@ class CarTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
         .isThrownBy(() -> {Car car = new Car("crongc");})
         .withMessageMatching("차의 이름을 5글자를 초과할 수 없습니다.");
+    }
+    
+    @Test
+    void Car_move_4이상_증가함() {
+        Car car = new Car("pobi");
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+    
+    @Test
+    void Car_move_4이하_증가안함() {
+        Car car = new Car("pobi");
+        car.move(3);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 
 }
