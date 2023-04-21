324df4cd5f2afa7dec1699c4439c201e5c85ca0a
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-21 12:14:08 +0900

    feat(Car): 멤버 변수 name을 가지는 Car 클래스 추가
    생성자를 통해 전달된 매개변수의 Length가 5 이상이 경우에만 멤버 변수 name에 할당합니다.

---
+++ java-racingcar-playground/src/main/java/racingcar/Car.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private String name;
+
+    public Car(String name) {
+        if( isInValidNameLength(name) ) {
+            throw new IllegalArgumentException("차의 이름을 5글자를 초과할 수 없습니다.");
+        }
+        this.name = name;
+    }
+
+    private boolean isInValidNameLength(String name) {
+        return name.length() > MAX_NAME_LENGTH;
+    }
+
+}
