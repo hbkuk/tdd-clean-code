c369db3f25b2d172003caf5bf0895b253bb592ad
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-24 21:51:35 +0900

    feat: Car 클래스의 hashCode(), equals() 메서드 오버라이드 및 생성자 오버로딩
    객체의 주소값을 비교가 아닌 값 비교를 위해서 hashCode(), equals() 메서드를 오버라이드 했습니다.
    테스트 가능한 구조로 변경하기 위해서 생성자를 오버로딩  했습니다.

--- java-racingcar-playground/src/main/java/racing/Car.java
+++ java-racingcar-playground/src/main/java/racing/Car.java
@@ -1,5 +1,7 @@
 package racing;
 
+import java.util.Objects;
+
 public class Car {
     private static final int MIN_FORWARD_VALUE = 4;
     private static final int MAX_NAME_LENGTH_VALUE = 5;
@@ -7,13 +9,17 @@ public class Car {
     private int position;
 
     public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int position) {
         if( StringUtils.isBlanck(name) ) 
             throw new IllegalArgumentException("1글자 이상을 입력해야 합니다.");
         
         if( isInvalidNameLegnth(name) )
             throw new IllegalArgumentException("5글자를 초과할 수 없습니다.");
-        
         this.name = name;
+        this.position = position;
     }
 
     private boolean isInvalidNameLegnth(String name) {
@@ -31,5 +37,22 @@ public class Car {
     
     public int getPosition() {
         return this.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Car other = (Car) obj;
+        return Objects.equals(name, other.name) && position == other.position;
     }
 }
