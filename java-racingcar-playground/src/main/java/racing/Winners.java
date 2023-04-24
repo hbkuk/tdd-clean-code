339132d5505c0ec14993d5a75b75a2d599e2a128
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-24 21:53:00 +0900

    feat: Car 리스트에서 우승자를 찾기 위한 Winners 클래스 추가
    Car 리스트를 순회하면서 최대 Position 값을 확인 후 재 순회하면서 List에 담는 로직을 추가했습니다.

---
+++ java-racingcar-playground/src/main/java/racing/Winners.java
@@ -0,0 +1,24 @@
+package racing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winners {
+
+    public static List<Car> findWinners(List<Car> cars) {
+        List<Car> winners = new ArrayList<>();
+        for( Car car : cars ) {
+            if( car.getPosition() == getMaxPositionValue(cars))
+                winners.add(car);
+        }
+        return winners;
+    }
+    private static int getMaxPositionValue(List<Car> cars) {
+        int maxPositionValue = 0;
+        for( Car car : cars ) {
+            if( car.getPosition() > maxPositionValue)
+                maxPositionValue = car.getPosition();
+        }
+        return maxPositionValue;
+    }
+}
