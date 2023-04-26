1577c7499b29304c51181535d6ec67859c3acd6f
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-26 15:27:18 +0900

    refactor(Coordinates): ENUM 추가에 따른 리팩토링

--- java-coordinate-playground/src/main/java/coordinate/Coordinates.java
+++ java-coordinate-playground/src/main/java/coordinate/Coordinates.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Coordinates {
     private List<Coordinate> coordinates;
+    private Shape Shape;
 
     public Coordinates(String line) {
         this.coordinates = initCoordinates(line);
@@ -16,7 +17,16 @@ public class Coordinates {
             String[] values = extractCoordinates(coordinate);
             coordinates.add( new Coordinate(toInt(values[0]), toInt(values[1])) );
         }
+        
+        if(isStraight(coordinates)) {
+            this.Shape = Shape.STRAIGHT;
+        }
+        
         return coordinates;
+    }
+
+    private boolean isStraight(List<Coordinate> coordinates) {
+        return coordinates.size() == 2;
     }
 
     private String[] extractCoordinates(String coordinate) {
@@ -33,5 +43,9 @@ public class Coordinates {
 
     public List<Coordinate> getCoordinates() {
         return this.coordinates;
+    }
+
+    public Shape getShape() {
+        return this.Shape;
     }
 }
--- java-coordinate-playground/src/test/java/coordinate/CoordinatesTest.java
+++ java-coordinate-playground/src/test/java/coordinate/CoordinatesTest.java
@@ -14,4 +14,11 @@ public class CoordinatesTest {
                     .contains(new Coordinate(10, 20), new Coordinate(14, 15) )
                     .doesNotContain(new Coordinate(10, 10));
     }
+    
+    @Test
+    void 두개의_좌표를_입력했을때_직선인지_확인() {
+        Coordinates coordinates = new Coordinates("(10,20)-(14,15)");
+        
+        assertThat(coordinates.getShape()).isEqualTo(Shape.STRAIGHT);
+    }
 }
