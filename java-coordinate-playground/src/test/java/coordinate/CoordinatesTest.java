dc97938043f23eb57032f8cce1081ff50c94b8d1
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-26 20:51:59 +0900

    test: 두 점 사이 거리 계산 메서드 관련 테스트 코드 추가
    소수점이 발생하는 경우 assertj의 offset() 활용해서 테스트를 진행했습니다.

--- java-coordinate-playground/src/test/java/coordinate/CoordinatesTest.java
+++ java-coordinate-playground/src/test/java/coordinate/CoordinatesTest.java
@@ -1,6 +1,7 @@
 package coordinate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,7 +19,27 @@ public class CoordinatesTest {
     @Test
     void 두개의_좌표를_입력했을때_직선인지_확인() {
         Coordinates coordinates = new Coordinates("(10,20)-(14,15)");
-        
         assertThat(coordinates.getShape()).isEqualTo(Shape.STRAIGHT);
+    }
+    
+    @Test
+    void 직선일때_두_점_사이의_거리_1() {
+        Coordinates coordinates = new Coordinates("(1,10)-(1,11)");
+        assertThat(coordinates.calculateDistance())
+                    .isEqualTo(1);
+    }
+    
+    @Test
+    void 직선일때_두_점_사이의_거리_2() {
+        Coordinates coordinates = new Coordinates("(10,1)-(10,11)");
+        assertThat(coordinates.calculateDistance())
+                    .isEqualTo(10);
+    }
+    
+    @Test
+    void 직선일때_두_점_사이의_거리_3() {
+        Coordinates coordinates = new Coordinates("(10,20)-(14,15)");
+        assertThat(coordinates.calculateDistance())
+                    .isEqualTo(6.403, offset(0.00099));
     }
 }
