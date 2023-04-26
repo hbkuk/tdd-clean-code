5b0fc31136e7d0f13a0f81433edfea2e4aab1246
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-26 13:46:36 +0900

    test: Coordinate 클래스를 테스트하는 테스트 코드 추가
    Coordinate 클래스 생성 시 유효성 검사가 제대로 이루어지는지 확인하는 테스트 코드를 추가했습니다.

---
+++ java-coordinate-playground/src/test/java/coordinate/CoordinateTest.java
@@ -0,0 +1,35 @@
+package coordinate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CoordinateTest {
+
+    @Test
+    void create() {
+        Coordinate coordinate = new Coordinate(1, 2);
+        assertThat(coordinate.getX()).isEqualTo(1);
+        assertThat(coordinate.getY()).isEqualTo(2);;
+    }
+    
+    @ParameterizedTest
+    @CsvSource({ "-1, 0", "0, -1", "-1, -1" })
+    void 음수로_설정할_경우(int x, int y) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> { Coordinate coordinate = new Coordinate(x, y); })
+                .withMessageMatching("좌표를 음수의 값으로 설정할 수 없습니다.");
+    }
+    
+    @ParameterizedTest
+    @CsvSource({ "25, 0", "25, 0", "25, 25" })
+    void 최대값인_24보다_큰_수로_설정할_경우(int x, int y) {
+        assertThatIllegalArgumentException()
+        .isThrownBy(() -> {Coordinate coordinate = new Coordinate(x, y);})
+        .withMessageMatching("좌표를 24보다 큰 값으로 설정할 수 없습니다.");
+    }
+    
+}
