056d1e4b8f89bf2355e861c79fd1a417192eeef6
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-05 14:33:54 +0900

    feat: x,y 좌표를 가지는 Point 클래스 추가

---
+++ java-coordinate-playground/src/main/java/coordinate/Point.java
@@ -0,0 +1,38 @@
+package coordinate;
+
+import java.util.Objects;
+
+public class Point {
+    public static final int MIN_POSITION_VALUE = 1;
+    public static final int MAX_POSITION_VALUE = 24;
+    public static final String NEW_CONSTUCTOR_EXCEPTION_MESSAGE =
+            String.format("입력범위를 초과했습니다. 입력범위는 %d이상 %d이하 입니다.",
+                                        MIN_POSITION_VALUE, MAX_POSITION_VALUE);
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        if( isInvalidNumber(x) || isInvalidNumber(y)) {
+            throw new IllegalArgumentException(NEW_CONSTUCTOR_EXCEPTION_MESSAGE);
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    private boolean isInvalidNumber(int number) {
+        return number < MIN_POSITION_VALUE || number > MAX_POSITION_VALUE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
