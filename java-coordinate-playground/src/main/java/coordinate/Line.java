68a66f5f8c882eb46f76ea7fbd5e944688282b56
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-08 00:03:05 +0900

    feat: Line 클래스 추가

--- java-coordinate-playground/src/main/java/coordinate/Line.java
+++ java-coordinate-playground/src/main/java/coordinate/Line.java
@@ -1,6 +1,14 @@
 package coordinate;
 
+import java.util.List;
+
 public class Line extends Figure {
+    public final static int LINE_POINTS_SIZE = 2;
+    private List<Point> points;
+
+    public Line(List<Point> points) {
+        this.points = points;
+    }
 
     @Override
     String getName() {
