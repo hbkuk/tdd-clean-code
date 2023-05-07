565c9bc1ccef0bb28e7072c4590f41d9c9a35e81
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-07 17:58:29 +0900

    feat: Line 클래스 추가

---
+++ java-coordinate-playground/src/main/java/coordinate/Line.java
@@ -0,0 +1,20 @@
+package coordinate;
+
+public class Line extends Figure {
+
+    @Override
+    String getName() {
+        return "Line";
+    }
+
+    @Override
+    int getSize() {
+        return 2;
+    }
+
+    @Override
+    double getArea() {
+        return 0;
+    }
+
+}
