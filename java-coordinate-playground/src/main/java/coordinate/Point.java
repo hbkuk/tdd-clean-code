ee14dc792462cb71d98208cb718785f5cb6b99ee
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-07 17:35:35 +0900

    feat: x와 y 값을 가지는 Position 클래스 추가

---
+++ java-coordinate-playground/src/main/java/coordinate/Point.java
@@ -0,0 +1,69 @@
+package coordinate;
+
+import java.util.Objects;
+
+public class Point {
+    private static final int MIX_POINT_VALUE = 1;
+    private static final int MAX_POINT_VALUE = 24;
+    private static final String CONSTRUCTOR_EXCEPTION_MESSAGE =
+                    String.format("%d 미만 또는 %d를 초과한 숫자를 입력할 수 없습니다.",
+                            MIX_POINT_VALUE, MAX_POINT_VALUE);
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        if(isInvalidNumber(x)) {
+            throw new IllegalArgumentException(CONSTRUCTOR_EXCEPTION_MESSAGE);
+        }
+        this.x = x;
+
+        if(isInvalidNumber(y)) {
+            throw new IllegalArgumentException("1 미만 또는 24를 초과한 숫자를 입력할 수 없습니다.");
+        }
+        this.y = y;
+    }
+
+    public int minusYAxis(Point otherPoint) {
+        return this.y - otherPoint.y;
+    }
+
+    public int minusXAxis(Point otherPoint) {
+        return this.x - otherPoint.x;
+    }
+
+    private boolean isInvalidNumber(int number) {
+        return number < MIX_POINT_VALUE || number > MAX_POINT_VALUE;
+    }
+
+    public double distance(Point otherPoint) {
+        int dx = minusXAxis(otherPoint);
+        int dy = minusYAxis(otherPoint);
+
+        return Math.sqrt(square(dx) + square(dy));
+    }
+
+    private int square(int number) {
+        return number * number;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
