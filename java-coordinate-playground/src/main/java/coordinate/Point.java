002d0a4c47a8ea654415ed225ffec8f0bea3034a
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-07 13:45:30 +0900

    feat: getter() 메서드 추가 및 same() 메서드 추가

--- java-coordinate-playground/src/main/java/coordinate/Point.java
+++ java-coordinate-playground/src/main/java/coordinate/Point.java
@@ -23,6 +23,18 @@ public class Point {
         return number < MIN_POSITION_VALUE || number > MAX_POSITION_VALUE;
     }
 
+    public boolean isSameY(int number) {
+        return this.y == number;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -35,4 +47,6 @@ public class Point {
     public int hashCode() {
         return Objects.hash(x, y);
     }
+
+
 }
