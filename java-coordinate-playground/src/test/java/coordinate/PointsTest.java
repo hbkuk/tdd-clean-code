d738f4186b69fe741817c9476e297770c954b02d
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-07 13:46:34 +0900

    test: 두 점 사이의 거리를 반환하는 메서드의 테스트 코드 추가

--- java-coordinate-playground/src/test/java/coordinate/PointsTest.java
+++ java-coordinate-playground/src/test/java/coordinate/PointsTest.java
@@ -1,5 +1,6 @@
 package coordinate;
 
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -7,6 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.offset;
 
 public class PointsTest {
 
@@ -22,6 +24,18 @@ public class PointsTest {
         assertThatIllegalArgumentException()
                             .isThrownBy(() -> {new Points(input);})
                             .withMessageMatching("좌표 사이의 구분자를 정확하게 입력해주세요.");
+    }
+
+    @Test
+    void distance_between_two_points() {
+        // given
+        Points points = new Points("(10,10)-(14,15)");
+
+        //when
+        double distance_between_two_points = points.getDistance();
+
+        //then
+        assertThat(distance_between_two_points).isEqualTo(6.403124, offset(0.000001d));
     }
 
 }
