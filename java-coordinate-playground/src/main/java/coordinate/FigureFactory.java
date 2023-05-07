79ab92b5034422032e889c8f4645fa5aa138639d
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-08 00:01:34 +0900

    feat: FigureFactory 클래스를 통해 선, 직사각형, 삼각형을 생성하도록 코드 추가

--- java-coordinate-playground/src/main/java/coordinate/FigureFactory.java
+++ java-coordinate-playground/src/main/java/coordinate/FigureFactory.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 public class FigureFactory {
     public static Figure getFigure(List<Point> points) {
-        return new Line();
+        if( points.size() == Line.LINE_POINTS_SIZE ) {
+            return new Line(points);
+        }
+
+        if( points.size() == Rectangle.RECTANGLE_POINTS_SIZE && Rectangle.isRectangle(points) ) {
+            return new Rectangle(points);
+        }
+
+        if( points.size() == Triangle.TRIANGLE_POINTS_SIZE && Triangle.isTriangle(points) ) {
+            return new Triangle(points);
+        }
+
+        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
     }
 }
